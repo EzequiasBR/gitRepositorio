29a87322538e74ebe6bc6cbc0a4f537a661053fc
Ezequias <ezequiashunk@gmail.com>
2024-05-04 19:27:26 -0300

    Update enviar_email.php

--- empresa/enviar_email.php
+++ empresa/enviar_email.php
@@ -17,14 +17,14 @@ try {
      $mail->CharSet = 'UTF-8';                                           //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'tectorruby@gmail.com';                     //SMTP username
-    $mail->Password   = 'vvog ytnr xbkw oaxx';                               //SMTP password
+    $mail->Username   = '';                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
     $mail->Port       = 587;
                                     //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('tectorruby@gmail.com', 'tectorruby');
+    $mail->setFrom('', 'tectorruby');
     $mail->addAddress($email, $nome);     //Add a recipient
 
     //Content
